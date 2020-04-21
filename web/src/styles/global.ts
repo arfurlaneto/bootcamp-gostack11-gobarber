72e606c3c5922ce5fe1ec5d1ce8a300087ab7527
arfurlaneto <arfurlaneto@gmail.com>
2020-04-21 20:32:51 -0300

    Login page layout.

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -14,7 +14,7 @@ export default createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
-  body, input, buttton {
+  body, input, button {
     font-family: 'Roboto Slab', serif;
     font-size: 16px;
   }
