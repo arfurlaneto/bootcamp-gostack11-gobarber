f803cd2c9eccdf7cc302bec405b570ff75868799
arfurlaneto <arfurlaneto@gmail.com>
2020-04-24 03:09:46 -0300

    Configuring navingation in mobile.

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Text>SignIn</Text>
+  </Container>
+);
+
+export default SignIn;
