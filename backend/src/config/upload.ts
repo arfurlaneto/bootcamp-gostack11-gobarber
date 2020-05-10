8fcaa80ee01c4fe635924b23178fd6de6487dca9
arfurlaneto <arfurlaneto@gmail.com>
2020-05-09 21:09:26 -0300

    Static files config fix.

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -6,10 +6,10 @@ const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
   tmpFolder,
-  uploadsFolder: path.resolve(tmpFolder, 'uploads');
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
   storage: multer.diskStorage({
-    destination: tmp,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('HEX');
       const fileName = `${fileHash}-${file.originalname}`;
