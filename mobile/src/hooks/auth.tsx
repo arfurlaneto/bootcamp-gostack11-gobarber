3abb591d138fe06433504447b1e9f0a1ca003068
arfurlaneto <arfurlaneto@gmail.com>
2020-07-03 22:19:22 -0300

    Mobile: listing available providers in dashboard.

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -40,14 +40,14 @@ const AuthProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
-      // await new Promise((resolve) => setTimeout(resolve, 5000));
-
       const [[, token], [, user]] = await AsyncStorage.multiGet([
         '@GoBarber:token',
         '@GoBarber:user',
       ]);
 
       if (token && user) {
+        api.defaults.headers.authorization = `Bearer ${token}`;
+
         setData({ token, user: JSON.parse(user) });
       }
 
@@ -66,6 +66,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
