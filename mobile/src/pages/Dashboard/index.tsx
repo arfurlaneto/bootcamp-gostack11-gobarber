d25f29b39f6ae9e5818cf139171cf246254c9989
arfurlaneto <arfurlaneto@gmail.com>
2020-07-03 21:43:13 -0300

    Mobile app: created dashboard header.

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View>
-      <Text>Dashboard</Text>
-      <Button onPress={signOut} title="Sair" />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
