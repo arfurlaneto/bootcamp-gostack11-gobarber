785ed57bc5213f00a3fe8a43f2501a0f4a54eb12
arfurlaneto <arfurlaneto@gmail.com>
2020-07-08 02:59:47 -0300

    Mobile: padding fixes.

--- mobile/src/pages/Profile/styles.ts
+++ mobile/src/pages/Profile/styles.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components/native';
-import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+  padding: 0 30px 40px;
 `;
 
 export const Title = styled.Text`
@@ -19,7 +18,7 @@ export const BackButton = styled.TouchableOpacity`
 `;
 
 export const UserAvatarButton = styled.TouchableOpacity`
-  margin-top: 64px;
+  margin-top: 0px;
 `;
 
 export const UserAvatar = styled.Image`
--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,12 +1,11 @@
 import styled from 'styled-components/native';
-import { Platform } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+  padding: 0 30px 40px;
 `;
 
 export const Title = styled.Text`
